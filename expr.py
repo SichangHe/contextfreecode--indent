0aeb9f1f4eb7ca825be65901ce389cedd4e4c4d0
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-06 07:17:56 +0800

    compact expr format for thumbnail

--- expr.py
+++ expr.py
@@ -14,34 +14,15 @@ class Categorized:
     category: str
 
 
-main = (
-    lambda: (
-        places := [
-            Place(name="Nuevo Progreso", population=2_704),
-            Place(name="San Marcos", population=47_063),
-            Place(name="Xela", population=180_706),
-        ]
-    )
-    and (
-        categorized_places := [
-            *map(
-                lambda place: (population := place.population)
-                and (
-                    category := (
-                        "village"
-                        if population < 5_000
-                        else "town"
-                        if population < 100_000
-                        else "city"
-                    )
-                )
-                and Categorized(name=place.name, category=category),
-                places,
-            )
-        ]
-    )
-    and pprint(categorized_places)
-)
-
-
-main() if __name__ == "__main__" else None
+(main := (lambda:
+    (places := [Place(name="Nuevo Progreso", population=2_704),
+        Place(name="San Marcos", population=47_063),
+        Place(name="Xela", population=180_706)])
+    and (categorized_places := [*map(
+        lambda place:
+            (population := place.population)
+            and (category := ("village" if population < 5_000 else "town" if population < 100_000 else "city"))
+            and Categorized(name=place.name, category=category),
+        places,)])
+    and pprint(categorized_places))
+) and main() if __name__ == "__main__" else None
