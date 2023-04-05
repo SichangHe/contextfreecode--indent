202361b9365818298ad3d85c56c51e96b4339cfe
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-05 22:41:24 +0800

    walrus operator option

--- indent.py
+++ indent.py
@@ -15,24 +15,12 @@ class Categorized:
 
 
 def main():
-    def categorize(place: Place) -> Categorized:
-        population = place.population
-        # Branch on population.
-        if population < 5_000:
-            category = "village"
-        elif population < 100_000:
-            category = "town"
-        else:
-            category = "city"
-        return Categorized(name=place.name, category=category)
-
     places = [
         Place(name="Nuevo Progreso", population=2_704),
         Place(name="San Marcos", population=47_063),
         Place(name="Xela", population=180_706),
     ]
 
-    # categorized_places = [*map(categorize, places)]
     categorized_places = [
         *map(
             lambda place: Categorized(
@@ -48,6 +36,44 @@ def main():
             places,
         )
     ]
+
+    ############################################################################
+    ############################# Using function. ##############################
+    ############################################################################
+
+    # def categorize(place: Place) -> Categorized:
+    #     population = place.population
+    #     # Branch on population.
+    #     if population < 5_000:
+    #         category = "village"
+    #     elif population < 100_000:
+    #         category = "town"
+    #     else:
+    #         category = "city"
+    #     return Categorized(name=place.name, category=category)
+    #
+    # categorized_places = [*map(categorize, places)]
+
+    ############################################################################
+    ######################### Using walrus operator. ###########################
+    ############################################################################
+
+    # categorized_places = [
+    #     *map(
+    #         lambda place: (population := place.population)
+    #         and (
+    #             category := (
+    #                 "village"
+    #                 if population < 5_000
+    #                 else "town"
+    #                 if population < 100_000
+    #                 else "city"
+    #             )
+    #         )
+    #         and Categorized(name=place.name, category=category),
+    #         places,
+    #     )
+    # ]
     pprint(categorized_places)
 
 
